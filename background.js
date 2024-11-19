b8784136c6d287cabcdad60981cf55d6d37c52a3
Aryan Baburajan <aryanbaburajan2007@gmail.com>
2024-11-19 20:57:16 +0530

    save-image: save images as png/jpg/svg

--- background.js
+++ background.js
@@ -1,7 +1,229 @@
+/* run settings initialization script on installation */
 chrome.runtime.onInstalled.addListener(function (object) {
-  let url = chrome.runtime.getURL("settings/settings.html");
+  let url = chrome.runtime.getURL("/settings/settings.html");
 
   if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     chrome.tabs.create({ url });
+  }
+});
+
+/* save-image: modification of the code from https://github.com/image4tools/Save-Image-as-Type/ as per GNU GPL v2 */
+let messages;
+
+// some old chrome doesn't support chrome.i18n.getMessage in service worker.
+if (!chrome.i18n?.getMessage) {
+  if (!chrome.i18n) {
+    chrome.i18n = {};
+  }
+  chrome.i18n.getMessage = (key, args) => {
+    if (key == "Save_as" && args?.[0]) {
+      return "Save as " + args[0];
+    }
+    return key;
+  };
+}
+
+function download(url, filename) {
+  chrome.downloads.download(
+    { url, filename, saveAs: true },
+    function (downloadId) {
+      if (!downloadId) {
+        let msg = chrome.i18n.getMessage("errorOnSaving");
+        if (chrome.runtime.lastError) {
+          msg += ": \n" + chrome.runtime.lastError.message;
+        }
+        notify(msg);
+      }
+    }
+  );
+}
+
+async function fetchAsDataURL(src, callback) {
+  if (src.startsWith("data:")) {
+    callback(null, src);
+    return;
+  }
+  fetch(src)
+    .then((res) => res.blob())
+    .then((blob) => {
+      if (!blob.size) {
+        throw "Fetch failed of 0 size";
+      }
+      let reader = new FileReader();
+      reader.onload = async function (evt) {
+        let dataurl = evt.target.result;
+        callback(null, dataurl);
+      };
+      reader.readAsDataURL(blob);
+    })
+    .catch((error) => callback(error.message || error));
+}
+
+function getSuggestedFilename(src, type) {
+  //special for chrome web store apps
+  if (src.match(/googleusercontent\.com\/[0-9a-zA-Z]{30,}/)) {
+    return "screenshot." + type;
+  }
+  if (src.startsWith("blob:") || src.startsWith("data:")) {
+    return "Untitled." + type;
+  }
+  let filename = src
+    .replace(/[?#].*/, "")
+    .replace(/.*[\/]/, "")
+    .replace(/\+/g, " ");
+  filename = decodeURIComponent(filename);
+  filename = filename.replace(/[\x00-\x7f]+/g, function (s) {
+    return s.replace(/[^\w\-\.\,@ ]+/g, "");
+  });
+  while (filename.match(/\.[^0-9a-z]*\./)) {
+    filename = filename.replace(/\.[^0-9a-z]*\./g, ".");
+  }
+  filename = filename.replace(/\s\s+/g, " ").trim();
+  filename = filename.replace(/\.(jpe?g|png|gif|webp|svg)$/gi, "").trim();
+  if (filename.length > 32) {
+    filename = filename.substr(0, 32);
+  }
+  filename = filename.replace(/[^0-9a-z]+$/i, "").trim();
+  if (!filename) {
+    filename = "image";
+  }
+  return filename + "." + type;
+}
+
+function notify(msg) {
+  if (msg.error) {
+    msg =
+      (chrome.i18n.getMessage(msg.error) || msg.error) +
+      "\n" +
+      (msg.srcUrl || msg.src);
+  }
+}
+
+function loadMessages() {
+  if (!messages) {
+    messages = {};
+    ["errorOnSaving", "errorOnLoading"].forEach((key) => {
+      messages[key] = chrome.i18n.getMessage(key);
+    });
+  }
+  return messages;
+}
+
+async function hasOffscreenDocument(path) {
+  const offscreenUrl = chrome.runtime.getURL(path);
+  const matchedClients = await clients.matchAll();
+  for (const client of matchedClients) {
+    if (client.url === offscreenUrl) {
+      return true;
+    }
+  }
+  return false;
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+  loadMessages();
+  ["JPG", "PNG", "WebP"].forEach(function (type) {
+    chrome.contextMenus.create({
+      id: "save_as_" + type.toLowerCase(),
+      title: "Save as " + type,
+      type: "normal",
+      contexts: ["image"],
+    });
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  let { target, op } = message || {};
+  if (target == "background" && op) {
+    if (op == "download") {
+      let { url, filename } = message;
+      download(url, filename);
+    } else if (op == "notify") {
+      let msg = message.message;
+      if (msg && msg.error) {
+        let msg2 = chrome.i18n.getMessage(msg.error) || msg.error;
+        if (msg.src) {
+          msg2 += "\n" + msg.src;
+        }
+        notify(msg2);
+      } else {
+        notify(message);
+      }
+    } else {
+      console.warn("unknown op: " + op);
+    }
+  }
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  let { menuItemId, mediaType, srcUrl } = info;
+  let connectTab = () => {
+    // for old chrome v108-
+    let port = chrome.tabs.connect(tab.id, {
+      name: "convertType",
+      frameId: info.frameId,
+    });
+    return port;
+  };
+  if (menuItemId.startsWith("save_as_")) {
+    if (mediaType == "image" && srcUrl) {
+      let type = menuItemId.replace("save_as_", "");
+      let filename = getSuggestedFilename(srcUrl, type);
+      loadMessages();
+      let noChange = srcUrl.startsWith(
+        "data:image/" + (type == "jpg" ? "jpeg" : type) + ";"
+      );
+      if (!chrome.offscreen) {
+        // for old chrome v108-
+        let frameIds = info.frameId ? [] : void 0;
+        await chrome.scripting.executeScript({
+          target: { tabId: tab.id, frameIds },
+          files: ["scripts/save-image-as-type.js"], // content script and offscreen use the same file.
+        });
+      }
+      fetchAsDataURL(srcUrl, async function (error, dataurl) {
+        if (error) {
+          notify({ error, srcUrl });
+          return;
+        }
+        // offscreen api need chrome v109+
+        if (!chrome.offscreen) {
+          // for old chrome v108-
+          let port = connectTab();
+          await port.postMessage({
+            op: noChange ? "download" : "convertType",
+            target: "content",
+            src: dataurl,
+            type,
+            filename,
+          });
+          return;
+        }
+        // for new chrome v109+
+        if (noChange) {
+          download(dataurl, filename);
+          return;
+        }
+        const offscreenSrc = "scripts/save-image-as-type.html";
+        if (!(await hasOffscreenDocument(offscreenSrc))) {
+          await chrome.offscreen.createDocument({
+            url: chrome.runtime.getURL(offscreenSrc),
+            reasons: ["DOM_SCRAPING"],
+            justification: "Download a image for user",
+          });
+        }
+        await chrome.runtime.sendMessage({
+          op: "convertType",
+          target: "offscreen",
+          src: dataurl,
+          type,
+          filename,
+        });
+      });
+      return;
+    } else {
+      notify(chrome.i18n.getMessage("errorIsNotImage"));
+    }
+    return;
   }
 });
---
+++ scripts/save-image.js
@@ -0,0 +1,116 @@
+// modification of the code from https://github.com/image4tools/Save-Image-as-Type/ as per GNU GPL v2
+
+// must use `var` instead of `let` to avoid of duplicated declared error when execute content script again
+var workAsContent, contentPort, listened, handleMessages;
+
+if (!listened) {
+  init();
+  listened = true;
+}
+
+function init() {
+  handleMessages = async (message) => {
+    let { op, target, filename, src, type } = message;
+    if (target !== "offscreen" && target !== "content") {
+      return false;
+    }
+    if (contentPort) {
+      contentPort.disconnect();
+      contentPort = null;
+    }
+    switch (op) {
+      case "convertType": {
+        if (!src || !src.startsWith("data:")) {
+          notify("Unexpected src");
+          return false;
+        }
+        convertImageAsType(src, filename, type);
+        break;
+      }
+      case "download": {
+        if (!src || !src.startsWith("data:")) {
+          notify("Unexpected src");
+          return false;
+        }
+        if (!workAsContent) {
+          notify("Cannot download on offscreen");
+          return false;
+        }
+        download(src, filename);
+        break;
+      }
+      default: {
+        console.warn(`Unexpected message type received: '${op}'.`);
+        return false;
+      }
+    }
+  };
+
+  // work as offscreen
+  chrome.runtime.onMessage.addListener(handleMessages);
+
+  // work as content script for old chrome (v108-)
+  chrome.runtime.onConnect.addListener((port) => {
+    if (port.name == "convertType") {
+      workAsContent = true;
+      contentPort = port;
+      port.onMessage.addListener(handleMessages);
+    }
+  });
+}
+
+function notify(message) {
+  if (workAsContent) {
+    alert(message);
+    return;
+  }
+  chrome.runtime.sendMessage({ op: "notify", target: "background", message });
+}
+
+function download(url, filename) {
+  if (workAsContent) {
+    let a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    return;
+  }
+  chrome.runtime.sendMessage({
+    op: "download",
+    target: "background",
+    url,
+    filename,
+  });
+}
+
+function convertImageAsType(src, filename, type) {
+  function getDataURLOfType(img, type) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var context = canvas.getContext("2d");
+    var mimeType = "image/" + (type == "jpg" ? "jpeg" : type);
+    context.drawImage(img, 0, 0);
+    var dataurl = canvas.toDataURL(mimeType);
+    canvas = null;
+    return dataurl;
+  }
+  function imageLoad(src, type, callback) {
+    var img = new Image();
+    img.onload = function () {
+      var dataurl = getDataURLOfType(this, type);
+      callback(dataurl);
+    };
+    img.onerror = function () {
+      notify({ error: "errorOnLoading", src });
+    };
+    img.src = src;
+  }
+  function callback(dataurl) {
+    download(dataurl, filename);
+  }
+  if (!src.startsWith("data:")) {
+  } else {
+    imageLoad(src, type, callback);
+  }
+}
